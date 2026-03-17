e49e1b1bcbff09a7f7de0736269c4413c2ef0c88
ValentinPastre <valentinpastre.uni@gmail.com>
2026-03-17 20:17:06 -0300

    cambios minimos al script de las ventanas, agrego nuevo wallpaper

--- .config/hypr/scripts/master-bspwm.sh
+++ .config/hypr/scripts/master-bspwm.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-# Función que maneja la lógica cuando se abre una ventana
-manejar_nueva_ventana() {
+manage_new_window() {
     local workspace_name="$1"
-    
     local window_count
+
     window_count=$(hyprctl clients -j | jq "[.[] | select(.workspace.name == \"$workspace_name\")] | length")
-    
+
     if (( window_count % 2 != 0 )); then
         hyprctl dispatch layoutmsg addmaster > /dev/null
     fi
 }
 
 socat -U - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do
-    
+
     if [[ "$line" == openwindow* ]]; then
-        
         payload="${line#*>>}"
-        
-        IFS=',' read -r address workspace_name basura <<< "$payload"
-        
-        manejar_nueva_ventana "$workspace_name"
+        IFS=',' read -r address workspace_name trash <<< "$payload"
+        manage_new_window "$workspace_name"
     fi
-    
 done
