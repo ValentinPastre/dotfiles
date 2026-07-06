18f5f586624beef5543028ea7d3df2fb24ec464c
ValentinPastre <valentinpastre.uni@gmail.com>
2026-07-06 19:49:17 -0300

    renamed waybar config file

--- .config/hypr/programs.lua
+++ .config/hypr/programs.lua
@@ -2,9 +2,9 @@ local programs = {
 	terminal = "kitty",
 	fileManager = "thunar",
 	menu = "rofi -config ~/.config/rofi/gruvbox-config.rasi -show drun",
-	webBrowser = "firefox",
+	webBrowser = "firefox || firefox-bin",
 	music = "kitty spotify_player",
-	statusBar = "waybar -c ~/.config/waybar/gruvbox-config.json -s ~/.config/waybar/gruvbox-style.css",
+	statusBar = "waybar -c ~/.config/waybar/minimal-config.json -s ~/.config/waybar/gruvbox-style.css",
 	logoutMenu = "wlogout --css ~/.config/wlogout/gruvbox-style.css -b 6 -T 325 -B 325",
 }
 
