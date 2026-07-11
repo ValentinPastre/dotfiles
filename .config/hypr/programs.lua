37d34dc3ee8ea5699d618c5eedf74cd2e24a2b5a
ValentinPastre <valentinpastre.uni@gmail.com>
2026-07-11 19:44:34 -0300

    migration to rofi-based wallpaper changer

--- .config/hypr/programs.lua
+++ .config/hypr/programs.lua
@@ -5,7 +5,7 @@ local programs = {
 	webBrowser = "firefox || firefox-bin",
 	music = "kitty spotify_player",
 	statusBar = "waybar -c ~/.config/waybar/minimal-config.json -s ~/.config/waybar/gruvbox-style.css",
-	logoutMenu = "wlogout --css ~/.config/wlogout/gruvbox-style.css -b 6 -T 325 -B 325",
+	logoutMenu = "wlogout --css ~/.config/wlogout/gruvbox-style.css -b 6 -T 325 -B 325 -P 0",
 }
 
 return programs
